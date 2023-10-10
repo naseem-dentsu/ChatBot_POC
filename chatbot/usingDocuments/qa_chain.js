d5ce591b3358448b9e66d69f9403f427da51e6d9
Naseem Khan <96776341+naseem-dentsu@users.noreply.github.com>
2023-10-10 16:43:22 +0530

    changes to prompts

--- chatbot/usingDocuments/qa_chain.js
+++ chatbot/usingDocuments/qa_chain.js
@@ -1,4 +1,5 @@
 import { configDotenv } from "dotenv";
+import { ChatOpenAI } from "langchain/chat_models/openai";
 import { OpenAI } from "langchain/llms/openai";
 import { ConversationalRetrievalQAChain } from "langchain/chains";
 import { BufferMemory } from "langchain/memory";
@@ -6,23 +7,26 @@ import constants from "../constants.js"
 configDotenv();
 
 const ChainPrompt = constants.ChainPrompt;
-const model = new OpenAI({
-});
+
 
 export default async function chainQueries(vectorStoreRetriever) {
-
-
+  const model = new ChatOpenAI({ temperature: 0.9 });
+  const baseModel = new OpenAI({ temperature: 0.9 });
   const chain = ConversationalRetrievalQAChain.fromLLM(
     model,
     vectorStoreRetriever,
     {
       memory: new BufferMemory({
         memoryKey: "chat_history",
+        // inputKey: "question", // The key for the input to the chain
+        // outputKey: "text", // The key for the final conversational output of the chain
         returnMessages: true,
       }),
       questionGeneratorChainOptions: {
         template: ChainPrompt,
+        llm: baseModel
       },
+      // returnSourceDocuments: true
     }
   );
 
