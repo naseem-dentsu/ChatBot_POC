4d5af34d3e76c6498dd707179bc233ad49b95cd2
Naseem Khan <96776341+naseem-dentsu@users.noreply.github.com>
2023-10-12 15:37:27 +0530

    ++typo fix

--- routes/api.js
+++ routes/api.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Api Page' });
+  res.send({ title: 'Api Page' });
 });
 
 //Using search results
